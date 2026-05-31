ed32dd369e2eec9ac1d398cdeebfa25a8ce317f4
Ping160Fps50 <alireza.shahnazarii@gmail.com>
2026-05-31 12:27:09 +0330

    Initialize Next.js project with essential configuration files and components. Added ESLint, PostCSS, Tailwind CSS, and basic app structure including pages, components, and a README. Configured routing and state management for user data display.

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>The Wild Oasis, Welcome To Paradise</h1>
+      <Link href="/cabins">Explore Luxury Cabins</Link>
+    </div>
+  );
+}
