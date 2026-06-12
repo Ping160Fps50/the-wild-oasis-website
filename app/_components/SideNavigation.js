52d5805a76c45a4a098677f882c6a131c7f54ec2
Ping160Fps50 <alireza.shahnazarii@gmail.com>
2026-06-12 17:32:25 +0330

    add filter capacity to cabins page

--- app/_components/SideNavigation.js
+++ app/_components/SideNavigation.js
@@ -1,12 +1,15 @@
-import Link from "next/link";
+"use client";
 
-import SignOutButton from "./SignOutButton";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 import {
   CalendarDaysIcon,
   HomeIcon,
   UserIcon,
 } from "@heroicons/react/24/solid";
+
+import SignOutButton from "./SignOutButton";
 
 const navLinks = [
   {
@@ -27,13 +30,15 @@ const navLinks = [
 ];
 
 function SideNavigation() {
+  const pathname = usePathname();
+
   return (
     <nav className="border-r border-primary-900">
       <ul className="flex flex-col gap-2 h-full text-lg">
         {navLinks.map((link) => (
           <li key={link.name}>
             <Link
-              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
+              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 ${pathname === link.href ? "bg-primary-900" : ""}`}
               href={link.href}
             >
               {link.icon}
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 
 import CabinList from "@/app/_components/CabinList";
 import Spinner from "@/app/_components/Spinner";
+import Filter from "@/app/_components/Filter";
 
 // export const revalidate = 3600;
 
@@ -9,7 +10,10 @@ export const metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
+export default async function Page({ searchParams }) {
+  const searchParameter = await searchParams;
+  const filter = searchParameter?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -24,8 +28,12 @@ export default function Page() {
         to paradise.
       </p>
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
