efe9555be04bd32b09e67a596ad840ff64cca28c
Ping160Fps50 <alireza.shahnazarii@gmail.com>
2026-06-14 19:28:50 +0330

    create datepicker for cabin page and create one next api

---
+++ app/_components/ReservationContext.js
@@ -0,0 +1,27 @@
+"use client";
+
+import { createContext, use, useState } from "react";
+
+const ReservationContext = createContext();
+
+const initialState = { from: null, to: null };
+
+function ReservationProvider({ children }) {
+  const [range, setRange] = useState(initialState);
+  const resetRange = () => setRange(initialState);
+
+  return (
+    <ReservationContext.Provider value={{ range, setRange, resetRange }}>
+      {children}
+    </ReservationContext.Provider>
+  );
+}
+
+function useReservation() {
+  const context = use(ReservationContext);
+  if (context === undefined)
+    throw new Error("Context Used Outside Of Provider!");
+  return context;
+}
+
+export { ReservationProvider, useReservation };
